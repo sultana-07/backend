fb9e80928c2877a284a089f9354b9e77df477f13
sultan-07 <sultanalih8@gmail.com>
2025-04-08 10:54:04 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import dontenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dontenv.config({
     path : './env'
 })
 
 connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 8000,() => {
+    console.log(`server is running at port : ${process.env.PORT}`);
+    
+  })
+})
+.catch((error) => {
+  console.log("mongodb connection error",error);
+  
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,8 @@
+const asynchandler = (requesthandler) => {
+     (req,res,next) => {
+          Promise.resolve(requesthandler(req,res,next))
+          .catch((err) => next(err))
+     }
+}
+
+export {asynchandler}
